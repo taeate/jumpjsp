2d35169be697d682038a94e1b9f1fcecfa2c25dd
taeate <yes3144@mate.com>
2022-04-18 21:20:22 +0900

    testJpa run failed

---
+++ src/main/java/com/exam/jumpjsp/demo/sbb/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.exam.jumpjsp.demo.sbb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
---
+++ src/main/java/com/exam/jumpjsp/demo/sbb/MainController.java
@@ -0,0 +1,14 @@
+package com.exam.jumpjsp.demo.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @RequestMapping("/main")
+    @ResponseBody
+    public String showMain() {
+        return "hi";
+    }
+}
