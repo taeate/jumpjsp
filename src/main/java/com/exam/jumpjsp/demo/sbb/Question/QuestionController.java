5427741dad264a99902ace984f72c0717fddd633
taeate <yes3144@mate.com>
2022-04-19 22:26:59 +0900

    list complate

--- src/main/java/com/exam/jumpjsp/demo/sbb/Question/QuestionController.java
+++ src/main/java/com/exam/jumpjsp/demo/sbb/Question/QuestionController.java
@@ -1,15 +1,27 @@
 package com.exam.jumpjsp.demo.sbb.Question;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RequestMapping("/article")
 @Controller
 public class QuestionController {
 
-    @RequestMapping("/question/list")
-    @ResponseBody
-    public String showList() {
-        return "question List";
+    private final QuestionRepository questionRepository;
+
+    @GetMapping("/list")
+    public String showList(Model model) {
+
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+
+        return "question/list";
     }
 }
